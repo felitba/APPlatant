ee60553379011956191c15612df879ef366e3d17
felitba <feli@itba.edu.ar>
2024-11-21 15:52:09 -0300

    add card files

--- app/src/main/java/ar/edu/itba/example/api/data/network/api/WalletApiService.kt
+++ app/src/main/java/ar/edu/itba/example/api/data/network/api/WalletApiService.kt
@@ -2,10 +2,23 @@ package ar.edu.itba.example.api.data.network.api
 
 import ar.edu.itba.example.api.data.network.model.NetworkWalletDetail
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface WalletApiService {
 
     @GET("wallet/details")
     suspend fun getDetails(): Response<NetworkWalletDetail>
+
+    @GET("wallet/cards")
+    suspend fun getCards(): Response<List<NetworkCard>>
+
+    @POST("wallet/cards")
+    suspend fun addCard(@Body card: NetworkCard): Response<NetworkCard>
+
+    @DELETE("wallet/cards/{cardId}")
+    suspend fun deleteCard(@Path("cardId") cardId: Int): Response<Unit>
 }
--- app/src/main/java/ar/edu/itba/example/api/data/repository/WalletRepository.kt
+++ app/src/main/java/ar/edu/itba/example/api/data/repository/WalletRepository.kt
@@ -1,9 +1,12 @@
 package ar.edu.itba.example.api.data.repository
 
+import ar.edu.itba.example.api.data.model.Card
 import ar.edu.itba.example.api.data.model.WalletDetail
 import ar.edu.itba.example.api.data.network.WalletRemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class WalletRepository(
     private val remoteDataSource: WalletRemoteDataSource
@@ -11,4 +14,38 @@ class WalletRepository(
     val walletDetailStream: Flow<WalletDetail> =
         remoteDataSource.walletDetailStream
             .map { it.asModel() }
+
+    //TODO: add the rest of the card logic.
+    //MAKE this Flow type
+    // Mutex to make writes to cached values thread-safe.
+//    private val cardsMutex = Mutex()
+//    // Cache of the latest sports got from the network.
+//    private var cards: List<Card> = emptyList()
+//
+//    suspend fun getCards(refresh: Boolean = false): List<Card> {
+//        if (refresh || cards.isEmpty()) {
+//            val result = remoteDataSource.getCards()
+//            // Thread-safe write to sports
+//            cardsMutex.withLock {
+//                this.cards = result.map { it.asModel() }
+//            }
+//        }
+//
+//        return cardsMutex.withLock { this.cards }
+//    }
+//
+//    suspend fun addCard(card: Card) : Card {
+//        val newCard = remoteDataSource.addCard(card.asNetworkModel()).asModel()
+//        cardsMutex.withLock {
+//            this.cards = emptyList()
+//        }
+//        return newCard
+//    }
+//
+//    suspend fun deleteCard(cardId: Int) {
+//        remoteDataSource.deleteCard(cardId)
+//        cardsMutex.withLock {
+//            this.cards = emptyList()
+//        }
+//    }
 }
