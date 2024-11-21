edc0cdec9a62a88b7f37f6fd149c79a0ce5d3b90
felitba <feli@itba.edu.ar>
2024-11-21 16:27:59 -0300

    adding teacher files

---
+++ app/src/main/java/ar/edu/itba/example/api/ui/home/ProfileScreen.kt
@@ -0,0 +1,26 @@
+package ar.edu.itba.example.api.ui.home
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
+import ar.edu.itba.example.api.R
+
+@Composable
+fun ProfileScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.profile),
+            fontSize = 30.sp
+        )
+    }
+}
